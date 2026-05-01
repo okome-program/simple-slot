1faa6ae5e8313ec08d9005fab3eb13013e7b1a02
okome-program <okome-program@outlook.jp>
2026-05-02 00:07:20 +0900

    Update main.js

--- the-slot/js/main.js
+++ the-slot/js/main.js
@@ -9,21 +9,42 @@ const ctx = canvas.getContext("2d");
 const img = new Image();
 img.src = "./assets/images/atlas.webp";
 
+let player_point = 5000;
+let drawbool = false;
+
 let slot_at = 0;
 let slot_bt = 0;
 let slot_ct = 0;
 
-img.onload = () => {
-  atlas_draw();
-};
+let slot_ap = 0;
+let slot_bp = 0;
+let slot_cp = 0;
 
-// draw function
-function atlas_draw() {
+let btn_p = 0;
+let btn_s = 0;
+
+
+img.onload = () => {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   ctx.fillStyle = "#ffffff";
   ctx.fillRect(16, 112, 256, 96);
   ctx.drawImage(img, 0, 0, 32, 32, 32, 128, 64, 64);
   ctx.drawImage(img, 0, 32, 32, 32, 112, 128, 64, 64);
   ctx.drawImage(img, 0, 64, 32, 32, 192, 128, 64, 64);
+};
+
+// draw function
+function atlas_draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "#ffffff";
+  ctx.fillRect(16, 112, 256, 96);
+  ctx.drawImage(img, 0, slot_ap, 32, 32, 32, 128, 64, 64);
+  ctx.drawImage(img, 0, slot_bp, 32, 32, 112, 128, 64, 64);
+  ctx.drawImage(img, 0, slot_cp, 32, 32, 192, 128, 64, 64);
+}
+
+function update() {
+  
 }
 
 const FPS = 20;
