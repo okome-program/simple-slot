60987685c92251eecf9a3256c9e27370e91ea0bb
okome-program <okome-program@outlook.jp>
2026-05-01 17:26:06 +0900

    Update main.js

--- the-slot/js/main.js
+++ the-slot/js/main.js
@@ -5,3 +5,12 @@ const ctx = canvas.getContext("2d");
 
 const img = new Image();
 img.src = "../assets/images/atlas.webp";
+
+img.onload = () => {
+  atlas_draw();
+} 
+
+// draw function
+function atlas_draw() {
+  ctx.drawImage(img, 0, 0, 32, 32, 0, 0, 32, 32);
+}
