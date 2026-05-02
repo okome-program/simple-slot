8d03926b3f16a924a29055d2dde85221e64a1ecb
okome-program <okome-program@outlook.jp>
2026-05-02 13:30:33 +0900

    Update main.js

--- the-slot/js/main.js
+++ the-slot/js/main.js
@@ -65,13 +65,13 @@ function update() {
     const slot_ap_math = Math.floor(slot_ap / 32) * 32;
     if (slot_at == 40) {
       slot_ap += 16;
-    }else if (slot_at > 0 && slot_at < 40) {
+		}else if (slot_at > 0 && slot_at < 40) {
       slot_ap += 8;
       slot_at--;
 		}else if (slot_at == 0 && slot_ap != slot_ap_math) {
       slot_ap += 8;
-  	}
-    if (slot_ap > 223) {
+		}
+		if (slot_ap > 223) {
       slot_ap = 0;
     }
     
