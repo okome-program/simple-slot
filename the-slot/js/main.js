b3f4b4788f4c5bf4cb2be3a68c68c967e9ac7f94
okome-program <okome-program@outlook.jp>
2026-05-03 22:20:54 +0900

    Update main.js

--- the-slot/js/main.js
+++ the-slot/js/main.js
@@ -36,7 +36,7 @@ function atlas_draw() {
   ctx.fillRect(16, 112, 256, 96);
   if (slot_ap > 192 ) { 
   	ctx.drawImage(img, 0, slot_ap, 32, 224 - slot_ap, 32, 128, 64, 2 * (224 - slot_ap));
-    ctx.drawImage(img, 0, 0, 32, 32 - (224 - slot_ap), 32, 128 + (224 - slot_ap), 64, 2 * (40 - (224 - slot_ap)));
+    ctx.drawImage(img, 0, 0, 32, 32 - (224 - slot_ap), 32, 128 + (224 - slot_ap), 64, 2 * (36 - (224 - slot_ap)));
   }else {
     ctx.drawImage(img, 0, slot_ap, 32, 32, 32, 128, 64, 64);
   }
